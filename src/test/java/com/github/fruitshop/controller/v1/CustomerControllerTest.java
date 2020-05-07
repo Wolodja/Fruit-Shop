e8e8cc04d6db6ffbd11deed691fe6d07741e189c
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-07 16:34:49 +0200

    Tests for customers added

--- src/test/java/com/github/fruitshop/controller/v1/CustomerControllerTest.java
+++ src/test/java/com/github/fruitshop/controller/v1/CustomerControllerTest.java
@@ -14,13 +14,16 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class CustomerControllerTest {
+class CustomerControllerTest extends AbstractRestControllerTest{
 
     @Mock
     CustomerService customerService;
@@ -50,5 +53,43 @@ class CustomerControllerTest {
         mockMvc.perform(get("/api/v1/categories/").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.customers", hasSize(2)));
+    }
+
+    @Test
+    public void testGetCustomerById() throws Exception {
+        //given
+        CustomerDto customer1 = new CustomerDto();
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        customer1.setCustomerUrl("/api/v1/customer/1");
+        when(customerService.findCustomerById(anyLong())).thenReturn(customer1);
+        //when
+        mockMvc.perform(get("/api/v1/customers/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", equalTo("Michale")));
+    }
+
+    @Test
+    public void createNewCustomer() throws Exception {
+        //given
+        CustomerDto customer = new CustomerDto();
+        customer.setFirstName("Fred");
+        customer.setLastName("Flintstone");
+
+        CustomerDto returnDTO = new CustomerDto();
+        returnDTO.setFirstName(customer.getFirstName());
+        returnDTO.setLastName(customer.getLastName());
+        returnDTO.setCustomerUrl("/api/v1/customers/1");
+
+        when(customerService.createNewCustomer(customer)).thenReturn(returnDTO);
+
+        //when/then
+        mockMvc.perform(post("/api/v1/customers/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(customer)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", equalTo("Fred")))
+                .andExpect(jsonPath("$.customer_url", equalTo("/api/v1/customers/1")));
     }
 }
---
+++ src/test/java/com/github/fruitshop/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,87 @@
+package com.github.fruitshop.service.impl;
+
+import com.github.fruitshop.domain.dto.CustomerDto;
+import com.github.fruitshop.domain.entity.Customer;
+import com.github.fruitshop.domain.mapper.CustomerMapper;
+import com.github.fruitshop.repository.CustomerRepository;
+import com.github.fruitshop.service.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+public class CustomerServiceImplTest {
+
+
+    @Mock
+    CustomerRepository customerRepository;
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+    CustomerService customerService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        customerService = new CustomerServiceImpl(customerRepository, customerMapper);
+    }
+    @Test
+    public void getAllCustomers() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstName("Sam");
+        customer2.setLastName("Axe");
+        when(customerRepository.findAll()).thenReturn(Arrays.asList(customer1, customer2));
+        //when
+        List<CustomerDto> customerDTOS = customerService.getAllCustomers();
+        //then
+        assertEquals(2, customerDTOS.size());
+    }
+    @Test
+    public void getCustomerById() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        when(customerRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(customer1));
+        //when
+        CustomerDto customerDTO = customerService.findCustomerById(1L);
+
+        assertEquals("Michale", customerDTO.getFirstName());
+    }
+
+    @Test
+    public void createNewCustomer() throws Exception {
+
+        //given
+        CustomerDto customerDTO = new CustomerDto();
+        customerDTO.setFirstName("Jim");
+
+        Customer savedCustomer = new Customer();
+        savedCustomer.setFirstName(customerDTO.getFirstName());
+        savedCustomer.setLastName(customerDTO.getLastName());
+        savedCustomer.setId(1l);
+
+        when(customerRepository.save(any(Customer.class))).thenReturn(savedCustomer);
+
+        //when
+        CustomerDto savedDto = customerService.createNewCustomer(customerDTO);
+
+        //then
+        assertEquals(customerDTO.getFirstName(), savedDto.getFirstName());
+        assertEquals("/api/v1/customers/1", savedDto.getCustomerUrl());
+    }
+}
